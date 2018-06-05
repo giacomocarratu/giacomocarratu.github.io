f2ba59c55322eb8b7cb787f9672c560f750d2bc1
giacomocarratu <39825639+giacomocarratu@users.noreply.github.com>
2018-06-05 17:46:25 +0200

    4

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-alert("Funziona");
+console.log("Start");
+
+jQuery(document).ready(
+        function() {
+
+        }
+
+        function createBox() {
+            jQuery("<div class='box'>CIAO SONO BOX</div>").appendTo()
+        }
